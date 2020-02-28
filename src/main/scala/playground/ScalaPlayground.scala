edf201624489cb8762001f0fe540642270bf0877
Jesse <folde01@users.noreply.github.com>
2020-02-28 20:11:31 +0000

    added recursive sum function with helper function in src/main/scala/playground/ScalaPlayground.scala

--- src/main/scala/playground/ScalaPlayground.scala
+++ src/main/scala/playground/ScalaPlayground.scala
@@ -2,4 +2,14 @@ package playground
 
 object ScalaPlayground extends App {
   println("hello scala")
+
+  def recursiveSum(ds: Seq[Double]): Double = {
+    def helper(seq: Seq[Double], sum: Double, index: Int): Double =
+      if (index == seq.length) sum
+      else helper(seq, sum + seq(index), index + 1)
+
+    helper(ds, 0, 0)
+  }
+
+  println(recursiveSum(Seq(1,2)))
 }
